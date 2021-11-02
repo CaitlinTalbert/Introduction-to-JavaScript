2ab636c62e96b5533b30a4ece197ff2cefeae701
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2021-11-02 15:52:02 -0500

    solved rock, paper, scissors

--- index.js
+++ index.js
@@ -152,8 +152,35 @@ RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors
 HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
 
-function game(user, computer){
-  /*add your code here*/
+let computer = Math.floor(Math.random() * 3); 
+if (computer === 0) {
+    computer = 'rock';
+  } else if (computer === 1) {
+    computer = 'paper'; 
+  } else if (computer === 2) {
+    computer = 'scissors';
+  }
+
+function game (user, computer){
+  if (user === 'rock' && computer === 'rock') {
+    return "it's a tie";
+  } else if (user === 'rock' && computer === 'paper') {
+    return "you lose!";
+  } else if (user === 'rock' && computer === 'scissors') {
+    return "you win!";
+  } else if (user === 'paper' && computer === 'rock') {
+    return "you win!";
+  } else if (user === 'paper' && computer === 'paper') {
+    return "it's a tie";
+  } else if (user === 'paper' && computer === 'scissors') {
+    return "you lose!";
+  } else if (user === 'scissors' && computer === 'rock') {
+    return "you lose!"; 
+  } else if (user === 'scissors' && computer === 'paper') {
+    return "you win!";
+  } else if (user === 'scissors' && computer === 'scissors') {
+    return "it's a tie";
+  }
 }
 
 
